58399628ec27e408a703e3a190da5ff39466f882
Vang Huynh <vanghm@imt-soft.com>
2021-07-24 10:34:42 +0700

    add project

---
+++ android/app/src/main/kotlin/com/vdev/flutter_form_login/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vdev.flutter_form_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form Login',
+      theme: ThemeData(
+        primaryColor: Colors.blue.shade100,
+      ),
+      home: Home(),
+    )
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Center(
+
+      ),
+    );
+  }
+
+}
